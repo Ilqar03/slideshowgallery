8a316c4181ed0ae37b8a3b35ab77ca3bbf7450f5
Ilqar03 <tu757r7k9@code.edu.az>
2022-12-26 11:50:23 +0400

    ss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react'
+
 function App() {
+  const imgs = [
+    { link: "https://www.viaero.com/images/thumbs/0000440_iphone-13-pro-max_550.jpeg" },
+    { link: "https://www.viaero.com/images/thumbs/0000116_iphone-13-pro_550.jpeg" },
+    { link: "https://www.viaero.com/images/thumbs/0000424_iphone-13-pro-max_550.jpeg" }
+  ]
+  const [displacement, setChange] = useState(imgs[0])
+  const imgsChange = (place) => {
+    const change = imgs[place]
+    setChange(change)
+    console.log(place)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='all'>
+      <div className='sss'>
+
+        <img src={displacement.link} />
+      </div>
+      <div className='imgdatas'>
+        {
+          imgs.map((data, value) => {
+            return (
+              <img src={data.link} onClick={() => imgsChange(value)} />
+            )
+          })
+        }
+      </div>
+      </div>
+      )
 }
 
-export default App;
+      export default App
